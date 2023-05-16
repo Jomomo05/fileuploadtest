de08bd44bbdec9ff8606b2f215b4fe5368574d05
Jose Miguel <69222925+Jomomo05@users.noreply.github.com>
2023-05-16 13:39:16 -0600

    Added Bootstrap

--- src/app/upload-form/upload-form.component.ts
+++ src/app/upload-form/upload-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-upload-form',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./upload-form.component.css']
 })
 export class UploadFormComponent {
+  selectedFile: null = null;
 
+  constructor(private http: HttpClient) { }
+
+  onFileSelected(event) {
+    this.selectedFile = <File>event.target.files[0];
+  }
+
+  onSubmit() {
+    const fd = new FormData();
+    fd.append('file', this.selectedFile, this.selectedFile.name);
+    this.http.post('http://localhost:3000/upload', fd)
+      .subscribe(res => {
+        console.log(res);
+      });
+  }
 }
