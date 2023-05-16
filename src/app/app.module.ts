da4caa72c77ecfa26219c60d5139f34a403e4ffd
Jose Miguel <69222925+Jomomo05@users.noreply.github.com>
2023-05-16 13:32:03 -0600

    created upload-form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UploadFormComponent } from './upload-form/upload-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadFormComponent
   ],
   imports: [
     BrowserModule,
