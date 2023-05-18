1de8620b46bf8f0c9635ef062d02aca182899ef8
Jose Miguel <69222925+Jomomo05@users.noreply.github.com>
2023-05-18 15:54:54 -0600

    Added login button

--- src/app/auth-button/auth-button.component.ts
+++ src/app/auth-button/auth-button.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+// Import the AuthService type from the SDK
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-auth-button',
-  templateUrl: './auth-button.component.html',
-  styleUrls: ['./auth-button.component.css']
+  //templateUrl: './auth-button.component.html',
+  //styleUrls: ['./auth-button.component.css']
+  template: '<button (click)="auth.loginWithRedirect()">Log in</button>'
 })
 export class AuthButtonComponent {
-
+  constructor(public auth: AuthService) {}
 }
